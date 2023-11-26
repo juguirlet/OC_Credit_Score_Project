19f409609c641972879c98ce69c876d8476de6cd
juguirlet <juliette.omont@outlook.fr>
2023-11-26 11:09:42 +0100

    Test streamlit sans matplotlib

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 import requests
 import json
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import shap
 import pickle
 import pandas as pd
