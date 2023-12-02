b60bd6f7d75aa51a35a2100b26b6721d2ba7d9ef
juguirlet <juliette.omont@outlook.fr>
2023-12-02 12:23:23 +0100

    test app

--- app/app.py
+++ app/app.py
@@ -51,6 +51,7 @@ def get_customer_shap_values(data_df):
     customer_values_array = scaled_data[0, :].reshape(1, -1)
     explainer = shap.TreeExplainer(classifier.steps[-1][1])
     shap_values = explainer.shap_values(customer_values_array)
+    shap_values = np.concatenate(shap_values, axis=0)
     return shap_values, customer_values_array, features_names
 
 def get_predicted_score(): #valeurs des variables
